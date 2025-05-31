da584b8782d16ae4f987c2fdf038b77343363dcd
TroYli <10245102431@stu.ecnu.edu.cn>
2025-06-01 00:08:03 +0800

    标题字体，旋转按钮和一些图片替换

--- pics_test.js
+++ pics_test.js
@@ -1,14 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('.button');
-    button.textContent = '旋转图片';
+    const prev_button = document.querySelector('.prev_button');
+    const next_button = document.querySelector('.next_button');
     const pic = document.querySelectorAll('.pic_item');
     console.log(pic);
+    const totalPages=pic.length;
     let rotation = 0;
-    button.addEventListener('click', function() {
-        rotation += 90;
-        pic.forEach((element, index) => {
-            const currentRotation = 90 * index;
-            element.style.transform = `rotateY(${currentRotation + rotation}deg) translateZ(350px)`; // 修改旋转角度
-        });
+    let currentPage=0;
+    prev_button.style.display = 'none';
+    prev_button.addEventListener('click', function() {
+        if(currentPage>0){
+            currentPage--;
+            rotation -= 90;
+            pic.forEach((element, index) => {
+                const currentRotation = 90 * index;
+                element.style.transform = `rotateY(${currentRotation + rotation}deg) translateZ(350px)`; // 修改旋转角度
+            });
+        }
+        prev_button.style.display = currentPage === 0 ? 'none' : 'block';
+        next_button.style.display = currentPage === totalPages - 1 ? 'none' : 'block';
+    });
+    next_button.addEventListener('click', function() {
+        if(currentPage<totalPages-1){
+            currentPage++;
+            rotation += 90;
+            pic.forEach((element, index) => {
+                const currentRotation = 90 * index;
+                element.style.transform = `rotateY(${currentRotation + rotation}deg) translateZ(350px)`; // 修改旋转角度
+            });
+        }
+        prev_button.style.display = currentPage === 0 ? 'none' : 'block';
+        next_button.style.display = currentPage === totalPages - 1 ? 'none' : 'block';
     });
 });
